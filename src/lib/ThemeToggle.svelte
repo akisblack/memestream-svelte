e40335c38731b23d7681656bb872a612e89b8f63
Akis <41871089+akisblack@users.noreply.github.com>
2022-10-08 12:04:37 +0300

    refactor upload (#1)
    * refactor upload
    
    * make form only show on index

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -15,10 +15,11 @@
 
 <DarkMode bind:theme />
 
-<button
-	class="button flex items-center gap-1"
-	on:click={toggle}
->
-	<i class="i-ic:outline-{theme === "dark" ? "light" : "dark"}-mode h-6 w-6 px-[.8rem]" />
+<button class="button flex items-center gap-1 justify-center" on:click={toggle}>
+	<i
+		class="i-ic:outline-{theme === 'dark'
+			? 'light'
+			: 'dark'}-mode h-6 w-6 px-[.8rem]"
+	/>
 	<span class="sm:hidden">toggle theme</span>
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,43 @@
 <script lang="ts">
-    let posts: any = [];
+	let posts: any = [];
 	async function fetchPosts() {
 		const url = `https://memestream.odyssey346.dev/api/v1/posts`;
 		const response = await fetch(url);
 		posts = await response.json();
 		return posts;
-    }
+	}
 </script>
 
-<div class="flex flex-basis-a justify-center">
-    <form action="https://memestream.odyssey346.dev/api/v1/upload" method="POST" enctype="multipart/form-data" class="flex flex-col sm:flex-row gap-4 p-4 mb-8 rounded bg-secondary">
-        <input type="file" accept=".jpg, .jpeg, .png, .gif, .mp4" name="file" class="button text-center cursor-pointer" required>
-        <button class="button">Submit</button>
-    </form>
-</div>
-
 {#await fetchPosts()}
-    <span>Loading posts...</span>
-{:then posts} 
-    <div class="flex flex-col-reverse items-center max-w-fit gap-8">
-        {#each posts as post}
-            <div class="border-4 border-accent rounded-2 mx-4">
-                {#if post.filetype === "image"}
-                    <img src="https://memestream.odyssey346.dev/{post.location}" alt="Post (img or GIF)" loading="lazy" class="rounded-1" />
-                    {:else}
-                        <video controls alt="Post (video)" loading="lazy" class="rounded-1">
-                            <source src="https://memestream.odyssey346.dev/{post.location}" type="video/mp4" />
-                            <track kind="captions" />
-                        </video>
-                {/if}
-            </div>
-        {/each}
-    </div>
-    {:catch}
-        <span>Failed to load posts.</span>
+	<span>Loading posts...</span>
+{:then posts}
+	<div class="flex flex-col-reverse items-center max-w-fit gap-8">
+		{#each posts as post}
+			<div class="border-4 border-accent rounded-2 mx-4">
+				{#if post.filetype === "image"}
+					<img
+						src="https://memestream.odyssey346.dev/{post.location}"
+						alt="Post (img or GIF)"
+						loading="lazy"
+						class="rounded-1"
+					/>
+				{:else}
+					<video
+						controls
+						alt="Post (video)"
+						loading="lazy"
+						class="rounded-1"
+					>
+						<source
+							src="https://memestream.odyssey346.dev/{post.location}"
+							type="video/mp4"
+						/>
+						<track kind="captions" />
+					</video>
+				{/if}
+			</div>
+		{/each}
+	</div>
+{:catch}
+	<span>Failed to load posts.</span>
 {/await}
-
-<style>
-    ::file-selector-button {
-        display: none;
-    }
-</style>
